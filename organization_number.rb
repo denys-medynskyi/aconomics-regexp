4dd697db9b7215d66820dff5b3c19954bfb115a3
Denys Medynskyi <denys.medynskyi@gmail.com>
2016-09-02 13:43:22 +0300

    regex for organization number

---
+++ organization_number.rb
@@ -0,0 +1,36 @@
+require_relative 'assert'
+regex = /(\b\d{9}\b)|(\b\d{3}\b\s\b\d{3}\b\s\b\d{3}\b)/
+# (\d{3}\s\d{3}\s\d{3})/
+# * 996678601
+# * 996 678 601
+
+# TEST
+example = '123456789'
+text = "erik #{example} anders"
+p assert(text.scan(regex).flatten.compact, [example])
+
+# TEST
+example = '1234567890'
+text = "erik #{example} anders"
+p assert(text.scan(regex).flatten.compact, [])
+
+# TEST
+example = '1'
+text = "erik #{example} anders"
+p assert(text.scan(regex).flatten.compact, [])
+
+# TEST
+example = '996 678 601'
+text = "erik #{example} anders"
+p assert(text.scan(regex).flatten.compact, [example])
+
+# TEST
+example = '996  678  601'
+text = "erik #{example} anders"
+p assert(text.scan(regex).flatten.compact, [])
+
+
+# TEST
+example = '9961 111 111'
+text = "erik #{example} anders"
+p assert(text.scan(regex).flatten.compact, [])
